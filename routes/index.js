244cc05e906e8a7c9e6f1781688650f19bddc398
akshat <akshatkumar000@gmail.com>
2023-04-28 21:44:25 +0530

    added code to load text

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,10 @@ var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('ER_tool', { title: 'Express' });
+  var json = fs.readFileSync('public/frontend_univ.json', 'utf8');
+  var text = JSON.parse(json);
+  text = JSON.stringify(text, null, 2);
+  res.render('ER_tool', { title: 'Express' ,text: text});
 });
 
 router.post('/er_to_sql', function(req, res, next) {
