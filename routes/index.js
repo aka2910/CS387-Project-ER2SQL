5f6572c7bb9b1602815dc64c18659920358b57b2
akshat <akshatkumar000@gmail.com>
2023-04-28 21:29:37 +0530

    reloaded default text

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var router = express.Router();
 var ER_to_SQL = require('../src/ER_to_SQL.js');
+var fs = require('fs');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('Er_tool', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+  // read text from frontend_univ.json
+  var json = await fs.readFileSync('public/frontend_univ.json', 'utf8');
+  var text = JSON.parse(json);
+  text = JSON.stringify(text, null, 2);
+  res.render('Er_tool', { title: 'Express' , text: text});
 });
 
 router.post('/er_to_sql', function(req, res, next) {
