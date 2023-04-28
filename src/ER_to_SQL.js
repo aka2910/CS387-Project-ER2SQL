c7b9fac1509f3aa1682e8578e68f30972a102233
Jash Kabra <200050054@iitb.ac.in>
2023-04-28 17:22:51 +0530

    Full Algo Done

--- src/ER_to_SQL.js
+++ src/ER_to_SQL.js
@@ -8,20 +8,11 @@ function ER_to_SQL(er){
     for(var i = 0; i < er.entities.length; i++){
         e = er.entities[i];
         var table = new Table(e.name);
-        if(!e.is_weak){
-            for(var j = 0; j < e.attributes.length; j++){
-                a = e.attributes[j];
-                table.addAttribute(a);
-                if(a.is_key){
-                    table.addPrimaryKey(a.name);
-                }
-            }
-        }
-        else{
-            for(var j = 0; j < e.attributes.length; j++){
-                a = e.attributes[j];
-                table.addAttribute(a);
-                table.addPrimaryKey(a.name);
+        for(var j = 0; j < e.attributes.length; j++){
+            attr = e.attributes[j];
+            table.addAttribute(attr);
+            if(attr.is_key){
+                table.addPrimaryKey(attr.name);
             }
         }
         // table.print_sql();
@@ -36,55 +27,72 @@ function ER_to_SQL(er){
             ident_entity = entity_map[r.e1.name];
             ident_table = table_map[ident_entity.name];
             for (var j=0; j<r.attributes.length; j++){
-                a = r.attributes[j];
-                weak_table.addAttribute(a);
-                if(a.is_key){
-                    weak_table.addPrimaryKey(a.name);
+                attr = r.attributes[j];
+                weak_table.addAttribute(attr);
+                if(attr.is_key){
+                    weak_table.addPrimaryKey(attr.name);
                 }
+                weak_entity.addAttribute(attr);
             }
             keys_list = [];
             for(var j = 0; j < ident_entity.attributes.length; j++){
-                a = ident_entity.attributes[j];
-                weak_table.addAttribute(a);
-                weak_table.addPrimaryKey(a.name);
-                keys_list.push(a.name);
+                attr = { ...ident_entity.attributes[j] };
+                if(attr.is_key){
+                    if(weak_table.attributes.map(attr1=>attr1.name).includes(attr.name)){
+                        attr.name = ident_entity.name+'_'+attr.name;
+                    }
+                    weak_table.addAttribute(attr);
+                    weak_table.addPrimaryKey(attr.name);
+                    weak_entity.addAttribute(attr);
+                    keys_list.push(attr.name);
+                }
                 
             }
             foreign = new Foreign_Key(keys_list, ident_table.name);
             weak_table.addForeignKey(foreign);
             table_map[weak_entity.name] = weak_table;
+            entity_map[weak_entity.name] = weak_entity;
         }
-        else{
+    }
+    for(var i = 0; i < er.relations.length; i++){
+        r = er.relations[i];
+        if(!r.is_weak){
         // Assuming partial participation for now
             if(r.c1=="many" && r.c2=="many"){
                 var table = new Table(r.name);
                 for(var j = 0; j < r.attributes.length; j++){
-                    a = r.attributes[j];
-                    table.addAttribute(a);
-                    if(a.is_key){
-                        table.addPrimaryKey(a.name);
+                    attr = r.attributes[j];
+                    table.addAttribute(attr);
+                    if(attr.is_key){
+                        table.addPrimaryKey(attr.name);
                     }
                 }
                 var e1 = entity_map[r.e1.name];
                 var e2 = entity_map[r.e2.name];
                 keys_list = [];
                 for (var j = 0; j < e1.attributes.length; j++) {
-                    a = e1.attributes[j];
-                    if (a.is_key) {
-                        table.addAttribute(a);
-                        table.addPrimaryKey(a.name);
-                        keys_list.push(a.name);
+                    attr = { ...e1.attributes[j]};
+                    if(table.attributes.map(attr1=>attr1.name).includes(attr.name)){
+                        attr.name = e1.name+'_'+attr.name;
+                    }
+                    if (attr.is_key) {
+                        table.addAttribute(attr);
+                        table.addPrimaryKey(attr.name);
+                        keys_list.push(attr.name);
                     }
                 }
                 foreign = new Foreign_Key(keys_list, e1.name);
                 table.addForeignKey(foreign);
                 keys_list = [];
                 for (var j = 0; j < e2.attributes.length; j++) {
-                    a = e2.attributes[j];
-                    if (a.is_key) {
-                        table.addAttribute(a);
-                        table.addPrimaryKey(a.name);
-                        keys_list.push(a.name);
+                    attr = e2.attributes[j];
+                    if(table.attributes.map(attr1=>attr1.name).includes(attr.name)){
+                        attr.name = e2.name+'_'+attr.name;
+                    }
+                    if (attr.is_key) {
+                        table.addAttribute(attr);
+                        table.addPrimaryKey(attr.name);
+                        keys_list.push(attr.name);
                     }
                 }
                 foreign = new Foreign_Key(keys_list, e2.name);
@@ -98,27 +106,72 @@ function ER_to_SQL(er){
                 var t2 = table_map[r.e2.name];
                 keys_list = [];
                 for(var j=0;j<t2.attributes.length;j++){
-                    var a = t2.attributes[j];
-                    if(a.is_key){
-                        t1.addAttribute(a);
-                        keys_list.push(a.name);
+                    var attr = { ... t2.attributes[j]};
+                    if(attr.is_key){
+                        if(t1.attributes.map(attr1=>attr1.name).includes(attr.name)){
+                            attr.name = e2.name+'_'+attr.name;
+                        }
+                        if(r.p1=='total' && r.p2=='partial'){
+                            attr.is_null=false;
+                        }
+                        attr.is_key=false;
+                        t1.addAttribute(attr);    
+                        keys_list.push('1'+attr.name);
                     }
                 }
                 foreign = new Foreign_Key(keys_list, t2.name);
                 t1.addForeignKey(foreign);
                 table_map[r.e1.name] = t1;
             }
-            else {
+            else if(r.c2=="many" && r.c1=="one"){
                 var e1 = entity_map[r.e1.name];
                 var e2 = entity_map[r.e2.name];
                 var t1 = table_map[r.e1.name];
                 var t2 = table_map[r.e2.name];
                 keys_list = [];
                 for(var j=0;j<t1.attributes.length;j++){
-                    var a = t1.attributes[j];
-                    if(a.is_key){
-                        t2.addAttribute(a);
-                        keys_list.push(a.name);
+                    var attr = { ...t1.attributes[j]};
+                    // console.log('1111111');
+                    // console.log(r.name);
+                    // console.log(attr);
+                    if(attr.is_key){
+                        if(t2.attributes.map(attr1=>attr1.name).includes(attr.name)){
+                            attr.name = e1.name+'_'+attr.name;
+                        }
+                        if(r.p2=='total' && r.p1=='partial'){
+                            attr.is_null=false;
+                        }
+                        attr.is_key=false;
+                        t2.addAttribute(attr);
+                        keys_list.push(attr.name);
+                    }
+                }
+                foreign = new Foreign_Key(keys_list, t1.name);
+                t2.addForeignKey(foreign);
+                table_map[r.e2.name] = t2;
+            }
+            else {
+                var e1 = entity_map[r.e1.name];
+                var e2 = entity_map[r.e2.name];
+                var t1 = table_map[r.e1.name];
+                var t2 = table_map[r.e2.name];
+                if(r.p1=='total' && r.p2=='partial'){
+                    t1,t2 = t2,t1
+                    e1,e2 = e2,e1
+                }
+                keys_list = [];
+                for(var j=0;j<t1.attributes.length;j++){
+                    var attr = { ...t1.attributes[j]};
+                    if(attr.is_key){
+                        if(t2.attributes.map(attr1=>attr1.name).includes(attr.name)){
+                            attr.name = e1.name+'_'+attr.name;
+                        }
+                        if((r.p2=='total' && r.p1=='partial')||(r.p1=='total' && r.p2=='partial')){
+                            attr.is_null=false;
+                        }
+                        attr.is_key=false;
+                        t2.addAttribute(attr);
+                        keys_list.push(attr.name);
                     }
                 }
                 foreign = new Foreign_Key(keys_list, t1.name);
@@ -137,6 +190,7 @@ function ER_to_SQL(er){
             table_map[er.relations[i].name].print_sql();
         }
     }
+
 }
 
 ER_to_SQL(er);
